7eb77c307f5382c328452367da934f64cc8974af
satyabharathi19 <148929278+satyabharathi19@users.noreply.github.com>
2025-05-16 16:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,23 @@ def course_recommender(course_list):
     return rec_course
   
 #connection = pymysql.connect(host=st.secrets.db_credintials.host,user=st.secrets.db_credintials.username, password=st.secrets.db_credintials.password,database=st.secrets.db_credintials.database)
-connection = pymysql.connect(host="localhost",user="MBharathi", password="Bharu@1234",database="resume")
+#connection = pymysql.connect(host="localhost",user="MBharathi", password="Bharu@1234",database="resume")
 #connection = pymysql.connect(**st.secrets.db_credintials)
+mysql_secrets = st.secrets["db_credentials"]
+
+host = mysql_secrets["host"]
+port = mysql_secrets["port"]
+database = mysql_secrets["database"]
+username = mysql_secrets["username"]
+password = mysql_secrets["password"]
+
+# Establish the database connection
+connection = pymysql.connect(
+    host=host,
+    user=username,
+    password=password,
+    database=database
+)
 
 cursor = connection.cursor()
 
